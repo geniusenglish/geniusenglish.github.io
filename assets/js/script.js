00f967db32f24769d7a310f6b843d9297507f799
geniusenglish <imjoy11111@gmail.com>
2026-07-07 17:53:22 +0900

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+// ===============================
+// GENIUS English Academy
+// JavaScript
+// ===============================
+
+
+// 로고 슬라이드
+
+const slides = document.querySelectorAll(".logo-slide");
+
+let current = 0;
+
+setInterval(() => {
+
+    slides[current].classList.remove("active");
+
+    current = (current + 1) % slides.length;
+
+    slides[current].classList.add("active");
+
+}, 3500);
