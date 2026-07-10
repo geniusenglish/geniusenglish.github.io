698dd04cf5d9515f037f4f5fa2b9adc544a7206a
geniusenglish <imjoy11111@gmail.com>
2026-07-10 21:40:49 +0900

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -68,17 +68,65 @@ if (bookGrid && typeof books !== "undefined") {
 
     <img src="${book.image}" alt="${book.title}">
 
-    <p>${book.title}</p>
+    <h3>${book.title}</h3>
 
-    <audio controls preload="none">
+    <button class="audio-btn">
+        🎧 음원 듣기
+    </button>
+
+    <audio preload="none">
         <source src="${book.audio}" type="audio/mpeg">
-        브라우저가 오디오를 지원하지 않습니다.
     </audio>
 
 </div>
 
-        `;
+`;
 
     });
 
 }
+
+
+/* 버튼 누르면 플레이어 열기 */
+
+document.addEventListener("click",function(e){
+
+    if(e.target.classList.contains("audio-btn")){
+
+        const card=e.target.parentElement;
+
+        const audio=card.querySelector("audio");
+
+        if(audio.style.display==="block"){
+
+            audio.pause();
+
+            audio.style.display="none";
+
+            e.target.innerHTML="🎧 음원 듣기";
+
+        }else{
+
+            document.querySelectorAll(".book audio").forEach(a=>{
+
+                a.pause();
+
+                a.style.display="none";
+
+            });
+
+            document.querySelectorAll(".audio-btn").forEach(btn=>{
+
+                btn.innerHTML="🎧 음원 듣기";
+
+            });
+
+            audio.style.display="block";
+
+            e.target.innerHTML="❌ 닫기";
+
+        }
+
+    }
+
+});
