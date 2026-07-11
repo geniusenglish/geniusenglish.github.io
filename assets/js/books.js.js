614d1749df718eb32b3876361bb95488b503ba52
geniusenglish <imjoy11111@gmail.com>
2026-07-11 18:03:00 +0900

    Add files via upload

---
+++ assets/js/books.js.js
@@ -0,0 +1,165 @@
+const books = [
+
+/* ===========================
+   AR 0
+=========================== */
+
+{
+    id:"big-egg",
+    level:0,
+    ar:"0.4",
+    title:"Big Egg",
+    image:"../images/ar0/ar0_bigegg.jpg",
+    likes:24
+},
+
+{
+    id:"cat-traps",
+    level:0,
+    ar:"0.5",
+    title:"Cat Traps",
+    image:"../images/ar0/ar0_cattraps.jpg",
+    likes:18
+},
+
+{
+    id:"the-go-kart",
+    level:0,
+    ar:"0.8",
+    title:"The Go-kart",
+    image:"../images/ar0/ar0_thegokart.jpg",
+    likes:31
+},
+
+
+/* ===========================
+   AR 2
+=========================== */
+
+{
+    id:"awesomeamphibians",
+    level:2,
+    ar:"2.0",
+    title:"Awesome Amphibians | Science Vocabulary Readers",
+    image:"../images/ar2/ar2_awesomeamphibians.jpg",
+    likes:24
+},
+
+{
+    id:"beautifulbirds",
+    level:2,
+    ar:"2.2",
+    title:"Beautiful Birds | Science Vocabulary Readers",
+    image:"../images/ar2/ar2_beautifulbirds.jpg",
+    likes:18
+},
+
+{
+    id:"chicklifecycle",
+    level:2,
+    ar:"2.1",
+    title:"Chick Life Cycle | Science Vocabulary Readers",
+    image:"../images/ar2/ar2_chicklifecycle.jpg",
+    likes:31
+},
+
+{
+    id:"froglifecycle",
+    level:2,
+    ar:"2.1",
+    title:"Frog Life Cycle | Science Vocabulary Readers",
+    image:"../images/ar2/ar2_froglifecycle.jpg",
+    likes:24
+},
+
+{
+    id:"honeyhunthelpers",
+    level:2,
+    ar:"2.8",
+    title:"Honey Hunt Helpers | The Berenstain Bears",
+    image:"../images/ar2/ar2_honeyhunthelpers.jpg",
+    likes:18
+},
+
+{
+    id:"justforyou",
+    level:2,
+    ar:"2.5",
+    title:"Just for You | Little Critter",
+    image:"../images/ar2/ar2_justforyou.jpg",
+    likes:31
+},
+
+{
+    id:"justgotobed",
+    level:2,
+    ar:"2.4",
+    title:"Just Go To Bed | Little Critter",
+    image:"../images/ar2/ar2_justgotobed.jpg",
+    likes:24
+},
+
+{
+    id:"ladybuglifecycle",
+    level:2,
+    ar:"2.2",
+    title:"Ladybug Life Cycle | Science Vocabulary Readers",
+    image:"../images/ar2/ar2_ladybuglifecycle.jpg",
+    likes:18
+},
+
+{
+    id:"sunflowerlifecycle",
+    level:2,
+    ar:"2.3",
+    title:"Sunflower Life Cycle | Science Vocabulary Readers",
+    image:"../images/ar2/ar2_sunflowerlifecycle.jpg",
+    likes:31
+},
+
+{
+    id:"thecoolcaterpillar",
+    level:2,
+    ar:"2.1",
+    title:"The Cool Caterpillar | Science Vocabulary Readers",
+    image:"../images/ar2/ar2_thecoolcaterpillar.jpg",
+    likes:24
+},
+
+{
+    id:"theearth",
+    level:2,
+    ar:"2.5",
+    title:"The Earth | Science Vocabulary Readers",
+    image:"../images/ar2/ar2_theearth.jpg",
+    likes:18
+},
+
+{
+    id:"themoon",
+    level:2,
+    ar:"2.4",
+    title:"The Moon | Science Vocabulary Readers",
+    image:"../images/ar2/ar2_themoon.jpg",
+    likes:31
+},
+
+{
+    id:"whenspringcomes",
+    level:2,
+    ar:"2.7",
+    title:"When Spring Comes",
+    image:"../images/ar2/ar2_whenspringcomes.jpg",
+    likes:31
+},
+
+{
+    id:"yourheart",
+    level:2,
+    ar:"2.2",
+    title:"Your Heart | Science Vocabulary Readers",
+    image:"../images/ar2/ar2_yourheart.jpg",
+    likes:31
+}
+
+];
